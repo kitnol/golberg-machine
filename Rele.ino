957551b8bfe85778a7446e10b5e5837671b10f74
kitnol <37303890+kitnol@users.noreply.github.com>
2023-05-02 13:36:13 +0300

    Update Rele.ino
    Added servo motor functions

--- Rele.ino
+++ Rele.ino
@@ -1,12 +1,16 @@
+#include <Servo.h>
 #define PIN_RELAY 5
 
+Servo myservo;
 const int buttonPin = 2;  // the number of the pushbutton pin
 int buttonState = 0;  // variable for reading the pushbutton status
+
 
 void setup() {
   pinMode(buttonPin, INPUT); //defening button as input
   pinMode(PIN_RELAY, OUTPUT); //defining relay as output
-  digitalWrite(PIN_RELAY, HIGH); //off relay
+  myservo.attach(9);
+	digitalWrite(PIN_RELAY, LOW); //off relay
   Serial.begin(9600);
 }
 
@@ -17,8 +21,10 @@ void loop() {
 
   if (buttonState == HIGH && turnedon == false) {
     turnedon = true;
-    digitalWrite(PIN_RELAY, LOW);
-    delay(5000);
     digitalWrite(PIN_RELAY, HIGH);
-  }
+    delay(5000);
+    digitalWrite(PIN_RELAY, LOW);
+		myservo.write(90);
+		delay(10000);
+	}
 }
